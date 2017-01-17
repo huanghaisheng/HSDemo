c234e606ebfe96a3e37579f643dc101c752c3dbe
haisheng huang <haisheng.huang@hotmail.com>
2017-01-17 19:29:40 +0800

    update

--- HSDemo/HSDemo/ViewController.swift
+++ HSDemo/HSDemo/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import HSCustomView
+import HSSwiftExtensions
 
 class ViewController: UIViewController {
 
+    var progress: HSCustomProgress?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let progressRect = CGRect(x: 15.0, y: 300.0, width: UIScreen.main.bounds.width - 30.0, height: 8.0)
+        progress = HSCustomView.createProgress(at: self.view, frame: progressRect, deepColor: .red, lightColor: .yellow, backgroundColor: UIColor.color(with: "0xf0f0f0"), value: 0.9, isCornerRadius: true, direction: .right, isAnimated: true, duration: 2.0)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 5.0) {
+            [unowned self] in
+            self.progress?.value = 0.3
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        DispatchQueue.global().asyncAfter(deadline: .now() + 15.0) {
+            [unowned self] in
+            self.progress?.value = 0.9
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
 
 }
 
